b1c1db80de093ffbbb2791b9d6b0a2091fd6334e
MugiranezaJ <jackswalter7@gmail.com>
2023-04-27 21:39:31 +0200

    fix: correct base urls

--- ehospital-frontend/src/utils/constants.js
+++ ehospital-frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-// export const SERVER_URL = "http://68.183.67.45:4200/api/v1";
-export const SERVER_URL = "http://localhost:4200/api/v1";
+export const SERVER_URL = "http://68.183.67.45:4200/api/v1";
+// export const SERVER_URL = "http://localhost:4200/api/v1";
